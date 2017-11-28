f5834a2a9103d0e022b106bf43b0576d5c3f50f4
Beanschen <2013499216@qq.com>
2017-11-28 22:12:12 +0800

    '1.0版本'

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # simple-rem
 移动rem自适应解决方案
+/////////////////////////////////
+> 需要在html页面创建一个id=viewport的meta标签
+
+## 改变分辨率
+
+	require('simple-rem')(640)
--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-module.exports=function(){
-	
+module.exports = function() {
+	var doc = document;
+	//设计稿分辨率
+	var rWidth = arguments[0] || 750;
+	//设置视口
+	var vMeta = doc.getElementById("viewport");
+	vMeta.name = "viewport";
+	vMeta.content = "width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no";
+	//设置缩放值
+	var docEl = doc.documentElement,
+		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+		recalc = function() {
+			var clientWidth = docEl.clientWidth;
+			if(!clientWidth) return;
+			clientWidth = (clientWidth > rWidth) ? rWidth : clientWidth;
+			docEl.style.fontSize = 100 * (clientWidth / rWidth) + 'px';
+		};
+	if(!doc.addEventListener) return;
+	window.addEventListener(resizeEvt, recalc, false);
+	recalc();
 };
